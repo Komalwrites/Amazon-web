9ce8fc9e4791a0ae485386a88f60357500613765
Komal <komal.professional.work@gmail.com>
2025-02-25 03:29:15 +0530

    Sign_in.js

--- client/src/components/sign-up_sign-in/Sign_in.js
+++ client/src/components/sign-up_sign-in/Sign_in.js
@@ -41,7 +41,7 @@ const history = useNavigate("");
           body: JSON.stringify({
               email, password
           }),
-          credentials: "include"
+          credentials: 'include'
       });
 
       const data = await res.json();
